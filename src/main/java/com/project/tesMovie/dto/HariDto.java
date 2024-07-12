3ceb651756fa6b2d0944a5bb88053bc45b21c6bd
bastian <bastiansetyo837@gmail.com>
2024-07-12 09:21:30 +0700

    feat : dto calender

---
+++ src/main/java/com/project/tesMovie/dto/HariDto.java
@@ -0,0 +1,17 @@
+package com.project.tesMovie.dto;
+
+import com.project.tesMovie.common.Day;
+import lombok.Data;
+
+
+@Data
+public class HariDto {
+    /* private Day month;*/
+
+    private Day day;
+    private Integer bulan;
+    private String date;
+   /* public Day getMonth(){
+        return month;
+    }*/
+ }
