5d3bf4fa00563518e215bf8e1428f43fd6af97fc
bastian <bastiansetyo837@gmail.com>
2024-07-12 09:24:53 +0700

    feat : testdto

---
+++ src/main/java/com/project/tesMovie/dto/TestDto.java
@@ -0,0 +1,10 @@
+package com.project.tesMovie.dto;
+
+import lombok.Data;
+
+@Data
+public class TestDto {
+    private int isNum1 = 0;
+    private int isNum2 = 0;
+    private int isProses = 0;
+}
