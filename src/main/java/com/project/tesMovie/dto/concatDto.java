65160ac406b585e9b5aa32eb36f961b7311f5e23
bastian <bastiansetyo837@gmail.com>
2024-07-12 09:22:18 +0700

    feat : dto concat

---
+++ src/main/java/com/project/tesMovie/dto/concatDto.java
@@ -0,0 +1,11 @@
+package com.project.tesMovie.dto;
+
+import lombok.Data;
+
+@Data
+public class concatDto {
+    private String input1 = "";
+    private String input2 = "";
+    private String input3 = "";
+
+}
